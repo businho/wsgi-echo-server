808565b9b3a1a6e0be70d9cec73cdb08bb6bb937
Iuri de Silvio <iurisilvio@gmail.com>
2023-02-27 13:17:52 -0300

    Run it without root and other small tweaks.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+if [ -z "$HTTP_PORT" ] && [ -z "$WSGI_PORT" ]; then
+    echo "ERROR: Set HTTP_PORT or WSGI_PORT."
+    exit 1
+fi
+
 if [ ! -z "$HTTP_PORT" ]; then
     _HTTP_SOCKET="--http-socket :$HTTP_PORT"
 fi
 if [ ! -z "$WSGI_PORT" ]; then
     _WSGI_SOCKET="--socket :$WSGI_PORT"
 fi
-exec uwsgi --module echo:app $_HTTP_SOCKET $_WSGI_SOCKET
+exec uwsgi \
+    --module "wsgi_echo_server:create_app()" \
+    --virtualenv .venv \
+    --uid nobody \
+    --gid nobody \
+    $_HTTP_SOCKET \
+    $_WSGI_SOCKET
